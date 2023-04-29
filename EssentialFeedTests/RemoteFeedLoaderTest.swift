8b7d02a1a10b9383cb75acb4c3b0bf92bbe7480b
Juan Carlos Merlos Albarracín <juancmal@ext.inditex.com>
2023-04-30 00:09:52 +0200

    Give a URL instead of hardcoding it in the RemoteFeecLoader

--- EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -11,13 +11,15 @@ import XCTest
 /// Creamos el nuesto tipo Remoto para `RemoteFeedLoaderTest`
 class RemoteFeedLoader {
     let client: HTTPClient
+    let url: URL
     
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
     
     func load() {
-        client.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: url)
     }
 }
 
@@ -38,8 +40,9 @@ final class RemoteFeedLoaderTest: XCTestCase {
     
     // Hacemos el mínimo test para el inicializador del caso de uso
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "https://a-url.com")!
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
+        _ = RemoteFeedLoader(url: url, client: client)
         
         XCTAssertNil(client.requestedURL)
     }
@@ -48,14 +51,15 @@ final class RemoteFeedLoaderTest: XCTestCase {
     func test_load_requestDataFromURL() {
         // Ahora sí tenemos un cliente, ya que traemos datos
         /// Arrange: `Given`, dado un cliente y un sut
+        let url = URL(string: "https://a.given-url.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(url: url, client: client)
         
         /// Act: `When` Cuando invocamos `sut.load()`
         sut.load()
         
         /// Assert: `Then` entonces afirmamos que una
         /// request URL fue iniciada en el `client`
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 }
