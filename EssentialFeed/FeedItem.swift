7fa1b94f9038f3a4918bce1a340dca62ece76afc
Juan Carlos Merlos Albarracín <juancmal@ext.inditex.com>
2023-04-29 21:58:50 +0200

    Create EssentialFeed Framework

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Juan Carlos Merlos Albarracin on 29/4/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUTL: URL
+}
