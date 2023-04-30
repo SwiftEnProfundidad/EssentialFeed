a741b03576e2b3c4d25e42608115331c044049dc
Juan Carlos Merlos Albarracín <juancmal@ext.inditex.com>
2023-04-30 11:02:12 +0200

    Warranty only receive one error

--- EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -46,10 +46,10 @@ final class RemoteFeedLoaderTest: XCTestCase {
         let (sut, client) = makeSUT()
         client.error = NSError(domain: "Test", code: 0)
         
-        var captureError: RemoteFeedLoader.Error?
-        sut.load { error in captureError = error }
+        var captureErrors = [RemoteFeedLoader.Error]()
+        sut.load { captureErrors.append($0) }
         
-        XCTAssertEqual(captureError, .connectivity)
+        XCTAssertEqual(captureErrors, [.connectivity])
     }
     
     // MARK: - Helpers - CÓDIGO DE TESTEO
