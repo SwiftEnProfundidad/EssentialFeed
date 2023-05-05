768975f788e873bb8a079eeda4d39c4a5e48622f
Juan Carlos Merlos <juan.merlos@globant.com>
2023-05-05 22:44:10 +0200

    Guarantees correct URL and HTTP method on get request

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
@@ -46,6 +46,28 @@ class URLSessionHTTPClient {
 
 final class URLSessionHTTPClientTest: XCTestCase {
     
+    // Caso de uso de obtener la URL y realizar una solicitud GET con la URL.
+    func test_getFromURL_performGETRequestWithURL() {
+        URLProtocolStub.startInterceptingRequest()
+        let url = URL(string: "http://any-url.com")!
+        let exp = expectation(description: "Wait for request")
+        // Creamos un método para observar las request y
+        // comparamos que la url y el httpMethod sea iguales
+        // a los que los dados. Es decir, garantizamos la URL
+        // y el método HTTP correctos en la solicitud GET
+        URLProtocolStub.observeRequest { request in
+            XCTAssertEqual(request.url, url)
+            XCTAssertEqual(request.httpMethod, "GET")
+            exp.fulfill()
+        }
+        
+        // Hacemos la solicitud desde una url dada.
+        URLSessionHTTPClient().get(from: url) { _ in }
+         
+        wait(for: [exp], timeout: 1.0)
+        URLProtocolStub.stopInterceptingRequest()
+    }
+    
     // Caso de uso en el que la URL falla en las solicitudes
     func test_getFromURL_failsOnRequestError() {
         // Necesitamos registrear `URLProtocolStub`
@@ -83,7 +105,7 @@ final class URLSessionHTTPClientTest: XCTestCase {
     
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
-        
+        private static var requestObserver: ((URLRequest) -> Void)?
         private struct Stub {
             let data: Data?
             let response: URLResponse?
@@ -94,6 +116,10 @@ final class URLSessionHTTPClientTest: XCTestCase {
             stub = Stub(data: data, response: response, error: error)
         }
         
+        static func observeRequest(observer: @escaping (URLRequest) -> Void) {
+            requestObserver = observer
+        }
+        
         static func startInterceptingRequest() {
             URLProtocol.registerClass(URLProtocolStub.self)
         }
@@ -102,6 +128,7 @@ final class URLSessionHTTPClientTest: XCTestCase {
             URLProtocol.unregisterClass(URLProtocolStub.self)
             // Removemos el stub interceptado al terminar
             stub = nil
+            requestObserver = nil
         }
         
         // Podemos ver `canInit` se llama como método de clase,
@@ -109,6 +136,7 @@ final class URLSessionHTTPClientTest: XCTestCase {
         // LOADING SYSTEM` instanciará nuestro `URLProtocolStub`
         // solo si podemos manejar la solicitud.
         override class func canInit(with request: URLRequest) -> Bool {
+            requestObserver?(request)
             // Interceptamos todas las solicitudes
             return true
         }
