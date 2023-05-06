fa89e8fae884be4320f4a5ad93a81b42d8ef1b15
Juan Carlos Merlos <juan.merlos@globant.com>
2023-05-06 11:32:26 +0200

    Move the URLSessionHTTPClient (the system under test, or SUT) creation to a factory method to protected our test from breaking changes

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
@@ -74,7 +74,7 @@ final class URLSessionHTTPClientTest: XCTestCase {
         }
         
         // Hacemos la solicitud desde una url dada.
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
          
         wait(for: [exp], timeout: 1.0)
     }
@@ -84,13 +84,11 @@ final class URLSessionHTTPClientTest: XCTestCase {
         let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
-        
-        let sut = URLSessionHTTPClient()
-        
+                
         let exp = expectation(description: "Wait for complition")
         
         // Aquí queremos la respuesta con un error
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
                 case let .failure(receivedError as NSError):
                     XCTAssertEqual(receivedError.domain, error.domain)
@@ -108,6 +106,13 @@ final class URLSessionHTTPClientTest: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    // Factory method: Movemos la creación de `URLSessionHTTPClient`
+    // (el sistema bajo prueba, o SUT) a un método `Factory` para
+    // proteger nuestra prueba de cambios importantes.
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
     
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
