62ef7c9b376b03c3e76c03cccc30c453a89eb955
Juan Carlos Merlos <juan.merlos@globant.com>
2023-05-09 11:32:02 +0200

    Save command does not request cache insertion on deletion error

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTest.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTest.swift
@@ -22,14 +22,20 @@ class LocalFeedLoader {
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+    var insertCallCount = 0
     
     func deleteCachedFeed() {
         deleteCachedFeedCallCount += 1
+    }
+    
+    func completeDeletion(with error: Error, at index: Int = 0) {
+        
     }
 }
 
 final class CacheFeedUseCaseTest: XCTestCase {
     
+    // Caso de uso en el que no se elimina la memoria cache al crearla
     func test_init_doesNotDeleteCacheUponCreation() {
         let (_ , store) = makeSUT()
         
@@ -45,6 +51,18 @@ final class CacheFeedUseCaseTest: XCTestCase {
         XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
     }
     
+    // Caso de uso en el que el guardado `save` solicita la
+    // inserción en caché y se complete con un error de eliminación.
+    func test_save_doesNotRequestCacheInsertionOnDeletionError() {
+        let items = [uniqueItems(), uniqueItems()]
+        let (sut, store) = makeSUT()
+        let deletionError = anyNSError()
+        
+        sut.save(items)
+        store.completeDeletion(with: deletionError)
+        
+        XCTAssertEqual(store.insertCallCount, 0)
+    }
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
@@ -63,5 +81,9 @@ final class CacheFeedUseCaseTest: XCTestCase {
     
     private func anyURL() -> URL {
         return URL(string: "http://any-url.com")!
+    }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 0)
     }
 }
