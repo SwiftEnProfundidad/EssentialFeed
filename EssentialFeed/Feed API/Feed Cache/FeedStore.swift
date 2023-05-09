22137cf29279e6127b87d9bfc5fbd64e3b014f70
Juan Carlos Merlos <juan.merlos@globant.com>
2023-05-10 00:03:15 +0200

    Move `FeedStore` to its own file

---
+++ EssentialFeed/Feed API/Feed Cache/FeedStore.swift
@@ -0,0 +1,17 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Juan Carlos Merlos Albarracin on 10/5/23.
+//
+
+import Foundation
+ 
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+    
+}
