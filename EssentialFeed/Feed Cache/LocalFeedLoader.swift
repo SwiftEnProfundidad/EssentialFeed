3a3f79380cb012330e42aaa057f4961e15d23cfc
Juan Carlos Merlos <juan.merlos@globant.com>
2023-05-16 11:29:05 +0200

    Extract local members into properties

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -10,6 +10,8 @@ import Foundation
 public final class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
+    private let calendar = Calendar(identifier: .gregorian)
+
     
     public typealias SaveResult = Error?
     public typealias LoadResult = LoadFeedResult
@@ -54,8 +56,11 @@ public final class LocalFeedLoader {
         }
     }
     
+    private var maxCacheAgeInDays: Int {
+        return 7
+    }
+    
     private func validate(_ timestamp: Date) -> Bool {
-        let calendar = Calendar(identifier: .gregorian)
         guard let maxCacheAge = calendar.date(byAdding: .day, value: 7, to: timestamp) else {
             return false
         }
