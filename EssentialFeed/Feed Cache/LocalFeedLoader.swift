86593bc443157d8bfed3d7a2145760d3a8c8d969
Merlos Albarracin Juan Carlos <x134262@santanderglobaltech.com>
2023-05-18 21:12:23 +0200

    Move `FeedCachePolicy` to its own file

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -7,29 +7,6 @@
 
 import Foundation
 
-private final class FeedCachePolicy {
-    // Hacemos este `init` private para que nadie puede hacer una
-    // instacia de ésta, ya que no necesita una identidad y no tiene estado.
-    private init() {}
-    
-    private static let calendar = Calendar(identifier: .gregorian)
-    
-    private static var maxCacheAgeInDays: Int {
-        return 7
-    }
-    
-    // Hacemos la función `static` ya que no tiene identidad y no necestiamos una instancia de `FeedCachePolicy`
-    // Esta `policy` es detereminista, no tiene `side effets` y no tiene estado, es solo una regla.
-    // Los objetos de valor son `Models` sin identidad. En este caso la `policy` no tiene identidad.
-    // Encapsula una regla que se puede reutilizar, lo que significa que no necesitamos una instancia de ésta.
-    static func validate(_ timestamp: Date, against date: Date) -> Bool {
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: 7, to: timestamp) else {
-            return false
-        }
-        return date < maxCacheAge
-    }
-}
-
 public final class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
