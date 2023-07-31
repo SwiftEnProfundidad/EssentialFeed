fb980119f624a84e344fff824c597d8f17b7ad4b
Merlos Albarracin Juan Carlos <juanca.merlos@servexternos.gruposantander.com>
2023-07-31 11:11:23 +0200

    Add EssentialFeedCacheIntegrationTests target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ EssentialFeed/Feed Cache/CoreData/ManageCache.swift
@@ -0,0 +1,33 @@
+//
+//  ManageCache.swift
+//  EssentialFeed
+//
+//  Created by Juan Carlos Merlos Albarracin on 30/7/23.
+//
+
+import CoreData
+
+@objc(ManagedCache)
+internal class ManagedCache: NSManagedObject {
+    @NSManaged internal var timestamp: Date
+    @NSManaged internal var feed: NSOrderedSet
+}
+
+extension ManagedCache {
+    internal static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+        let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
+        request.returnsObjectsAsFaults = false
+        return try context.fetch(request).first
+    }
+
+    internal static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+        try find(in: context).map(context.delete)
+        return ManagedCache(context: context)
+    }
+
+    internal var localFeed: [LocalFeedImage] {
+        return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
+    }
+}
+
+
