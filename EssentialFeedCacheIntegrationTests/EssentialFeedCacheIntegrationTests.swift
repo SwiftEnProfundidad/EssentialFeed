b0a5eae7bc3d4e7ae4413c413c0bb69c3e1101b2
Merlos Albarracin Juan Carlos <juanca.merlos@servexternos.gruposantander.com>
2023-07-31 12:56:20 +0200

    Clean up & undo all cache side-effects on setUp and tearDown to avoid sharing state between tests

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -8,8 +8,20 @@
 import XCTest
 import EssentialFeed
 
-final class EssentialFeedCacheIntegrationTests: XCTestCase {
-
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        setupEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        undoStoreSideEffects()
+    }
+    
     // Caso en el que se está tratando de cargar desde una caché vacía
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
@@ -17,11 +29,11 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
         let exp = expectation(description: "Wait for load completion")
         sut.load { result in
             switch result {
-                case let .success(imageFeed):
-                    XCTAssertEqual(imageFeed, [], "Expected empty feed")
+                case let .success(loadedFeed):
+                    XCTAssertEqual(loadedFeed, expectedFeed, file: file, line: line)
                     
                 case let .failure(error):
-                    XCTFail("Expected successful feed resutl, got \(error) instead")
+                    XCTFail("Expected successful feed result, got \(error) instead", file: file, line: line)
             }
             
             exp.fulfill()
@@ -29,17 +41,16 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
-    // MARK: - Helpers
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
     
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
-        let storeBundle = Bundle(for: CoreDataFeedStore.self)
-        let storeURL = testSpecificStoreURL()
-        let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
-        // SUT(LocalFeedLoader) en colaboración con CoreData para esta prueba de integración
-        let sut = LocalFeedLoader(store: store, currentDate: Date.init)
-        trackForMemoryLeaks(store, file: file, line: line)
-        trackForMemoryLeaks(sut, file: file, line: line)
-        return sut
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
     
     // En estos dos métodos, a diferencia de los Unit Test, es que usamos
@@ -51,5 +62,5 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
     private func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
     }
+    
 }
-
