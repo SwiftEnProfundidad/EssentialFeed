1f525d6e24b9dfc54ac4b94257f22a06e033cf04
Merlos Albarracin Juan Carlos <juanca.merlos@servexternos.gruposantander.com>
2023-07-31 12:03:30 +0200

    Include memory leak tracking helper in the cache integration tests target

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,54 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Juan Carlos Merlos Albarracin on 31/7/23.
+//
+
+import XCTest
+import EssentialFeed
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+    // Caso en el que se está tratando de cargar desde una caché vacía
+    func test_load_deliversNoItemsOnEmptyCache() {
+        let sut = makeSUT()
+        
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result {
+                case let .success(imageFeed):
+                    XCTAssertEqual(imageFeed, [], "Expected empty feed")
+                    
+                case let .failure(error):
+                    XCTFail("Expected successful feed resutl, got \(error) instead")
+            }
+            
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let storeURL = testSpecificStoreURL()
+        let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+        // SUT(LocalFeedLoader) en colaboración con CoreData para esta prueba de integración
+        let sut = LocalFeedLoader(store: store, currentDate: Date.init)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+    // En estos dos métodos, a diferencia de los Unit Test, es que usamos
+    // una URL real de File System para guardar nuestros modelos de Core Data
+    private func testSpecificStoreURL() -> URL {
+        return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    private func cachesDirectory() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+}
