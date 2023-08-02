3d8cce8da9bedd09734aecf975301913da510585
Juan Carlos Merlos Albarracín <x134262@santanderglobaltech.com>
2023-08-03 00:31:36 +0200

    Add prototype feed data for demoing purpose

---
+++ Prototype/Prototype/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Prototype
+//
+//  Created by Juan Carlos Merlos Albarracin on 2/8/23.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Prototype/Prototype/FeedViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FeedViewController.swift
+//  Prototype
+//
+//  Created by Juan Carlos Merlos Albarracin on 2/8/23.
+//
+
+import UIKit
+
+struct FeedImageViewModel {
+    let description: String?
+    let location: String?
+    let imageName: String
+}
+
+final class FeedViewController: UITableViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "FeedImageCell")!
+    }
+}
