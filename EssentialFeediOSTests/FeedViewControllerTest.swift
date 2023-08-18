7e45af0fd04a4e7eb307a87f42a803ca233aa7a7
Juan Carlos Merlos Albarracín <merlosalbarracin@gmail.com>
2023-08-18 12:01:18 +0200

    Hide loading indicator on loader completion

--- EssentialFeediOSTests/FeedViewControllerTest.swift
+++ EssentialFeediOSTests/FeedViewControllerTest.swift
@@ -29,7 +29,9 @@ final class FeedViewController: UITableViewController {
     }
     
     @objc private func load() {
-        loader?.load { _ in }
+        loader?.load { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
     }
 }
 
@@ -63,12 +65,22 @@ final class FeedViewControllerTest: XCTestCase {
     }
     
     // Caso de uso en el que primero cargamos los Feed's y aseguramos de tener un indicador de carga cuando se carga la view
+    // Mostrar spinner en viewDidLoad
     func test_viewDidLoad_showsLoadingIndicator() {
         let (sut, _) = makeSUT()
         
         sut.loadViewIfNeeded()
         
         XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
+    }
+    
+    func test_viewDidLoad_hidesLoadingIndicatorOnLoaderCompletion() {
+        let (sut, loader) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading()
+        
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
     }
     
     // MARK: - Helpers
@@ -83,12 +95,19 @@ final class FeedViewControllerTest: XCTestCase {
     }
     
     class LoaderSpy: FeedLoader {
-        private(set) var loadCallCount: Int = 0
+        private var completions = [(FeedLoader.Result) -> Void]()
+        
+        var loadCallCount: Int {
+            return completions.count
+        }
 
         func load(completion: @escaping (FeedLoader.Result) -> Void) {
-            loadCallCount += 1
+            completions.append(completion)
         }
         
+        func completeFeedLoading() {
+            completions[0](.success([]))
+        }
     }
 }
 
