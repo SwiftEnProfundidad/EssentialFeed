0eb49b26e674c8be1bc757c68899a6b97ee7acd8
Juan Carlos Merlos Albarracín <merlosalbarracin@gmail.com>
2023-08-21 22:44:35 +0200

    Extract adapter pattern into a separate function to clarify intent

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -14,11 +14,20 @@ public final class FeedUIComposer {
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let refreshController = FeedRefreshViewController(feedLoader: feedLoader)
         let feedController = FeedViewController(refreshController: refreshController)
-        refreshController.onRefresh = { [weak feedController] feed in
-            feedController?.tableModel = feed.map { model in
-                FeedImageCellController(model: model, imageLoader: imageLoader)
+        refreshController.onRefresh = adaptFeedToCellControllers(forwardingTo: feedController, loader: imageLoader)
+        return feedController
+    }
+    
+    // [FeedImage] transforma este array o -> Adapt -> en un array de [FeedImageCellController]
+    // Este closure es un "Adapter pattern", muy común en los tipos `Composer`
+    // Este patrón nos ayuda a conectar APIs inigualables como en este caso,
+    // dado que `onRefresh` es un array de `FeedImage` y `tableModel` es un
+    // array de `FeedImageCellController`
+    private static func adaptFeedToCellControllers(forwardingTo controller: FeedViewController, loader: FeedImageDataLoader) -> ([FeedImage]) -> Void {
+        return { [weak controller] feed in
+            controller?.tableModel = feed.map { model in
+                FeedImageCellController(model: model, imageLoader: loader)
             }
         }
-        return feedController
     }
 }
