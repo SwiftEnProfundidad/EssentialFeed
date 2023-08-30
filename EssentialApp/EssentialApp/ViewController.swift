605bfee35fa0115943b66497c50fe6be5e03e908
Juan Carlos Merlos Albarracín <merlosalbarracin@gmail.com>
2023-08-31 00:00:35 +0200

    Add empty EssentialApp project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Juan Carlos Merlos Albarracin on 30/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
