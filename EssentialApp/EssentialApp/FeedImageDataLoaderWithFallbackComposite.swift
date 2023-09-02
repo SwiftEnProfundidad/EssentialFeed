4dfed07cef77898358bb1ebef684d75d474cffc8
Juan Carlos Merlos Albarracín <merlosalbarracin@gmail.com>
2023-09-02 12:11:04 +0200

    FeedLoaderCacheDecorator.load delivers decoratee loader result (either success or failure)

---
+++ EssentialApp/EssentialApp/FeedImageDataLoaderWithFallbackComposite.swift
@@ -0,0 +1,8 @@
+//
+//  FeedImageDataLoaderWithFallbackComposite.swift
+//  EssentialApp
+//
+//  Created by Juan Carlos Merlos Albarracin on 1/9/23.
+//
+
+import Foundation
---
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTest.swift
@@ -0,0 +1,8 @@
+//
+//  FeedLoaderCacheDecoratorTest.swift
+//  EssentialAppTests
+//
+//  Created by Juan Carlos Merlos Albarracin on 2/9/23.
+//
+
+import Foundation
