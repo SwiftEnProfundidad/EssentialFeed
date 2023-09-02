019e7122d04b742450f86e57fc780ce31d74f389
Juan Carlos Merlos Albarracín <merlosalbarracin@gmail.com>
2023-09-02 13:11:38 +0200

    Extract FeedLoader test helpers into a shared protocol extension toremove duplication

--- EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTest.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTest.swift
@@ -22,7 +22,7 @@ final class FeedLoaderCacheDecorator: FeedLoader {
     }
 }
 
-class FeedLoaderCacheDecoratorTest: XCTestCase {
+class FeedLoaderCacheDecoratorTest: XCTestCase, FeedLoaderTestCase {
     
     func test_load_deliversFeedOnLoaderSucces() {
         let feed = uniqueFeed()
@@ -41,24 +41,4 @@ class FeedLoaderCacheDecoratorTest: XCTestCase {
     
     // MARK: - Helpers
     
-    private func expect(_ sut: FeedLoader, toCompleteWith expectedResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) {
-        let exp = expectation(description: "Wait for load completion")
-        
-        sut.load { receivedResult in
-            switch (receivedResult, expectedResult) {
-                case let (.success(receivedFeed), .success(expectedFeed)):
-                    XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
-                    
-                case (.failure, .failure):
-                    break
-                    
-                default:
-                    XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
-            }
-            
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1.0)
-    }
 }
--- EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTest.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTest.swift
@@ -9,7 +9,7 @@ import XCTest
 import EssentialFeed
 import EssentialApp
 
-class FeedLoaderWithFallbackCompositeTest: XCTestCase {
+class FeedLoaderWithFallbackCompositeTest: XCTestCase, FeedLoaderTestCase {
     
     func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
         let primaryFeed = uniqueFeed()
@@ -38,57 +38,13 @@ class FeedLoaderWithFallbackCompositeTest: XCTestCase {
     private func makeSUT(primaryResult: FeedLoader.Result, fallbackResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) -> FeedLoader {
         // Con este `Stub` (`LoaderStub`), protegemos esta prueba de futuros cambios.
         // Estamos testeando `RemoteWithLocalFallbackFeedLoader` de forma aislada.
-        let primaryLoader = LoaderStub(result: primaryResult)
-        let fallbackLoader = LoaderStub(result: fallbackResult)
+        let primaryLoader = FeedLoaderStub(result: primaryResult)
+        let fallbackLoader = FeedLoaderStub(result: fallbackResult)
         let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
         trackForMemoryLeaks(primaryLoader, file: file, line: line)
         trackForMemoryLeaks(fallbackLoader, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
-    }
-    
-    private func expect(_ sut: FeedLoader, toCompleteWith expectedResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) {
-        // Como estamos capturando un valor, necesitamos una expectativa
-        let exp = expectation(description: "Wait for load completion")
-        
-        // Para capturar el feed recibido, primero debemos invocar a `load` en `sut` y obtener el `Result`
-        sut.load { receivedResult in
-            // Aquí capturamos el `Feed` recibido
-            switch (receivedResult, expectedResult) {
-                case let (.success(receivedFeed), .success(expectedFeed)):
-                    // Esperamos cargar un `Feed`, así que comparamos un feed recibido con un `Result`, un `RemoteFeed`
-                    XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
-                    
-                case (.failure, .failure):
-                    break
-                    
-                default:
-                    XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
-            }
-            
-            // Cumplimos aquí la expectativa
-            exp.fulfill()
-        }
-        
-        // Y esperamos la expectativa con un tiempo de espera para
-        // asegurarnos que al finalizar la prueba se ejecutó el closure
-        wait(for: [exp], timeout: 1.0)
-    }
-    
-    private func uniqueFeed() -> [FeedImage] {
-        return [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "http://any-url.com")!)]
-    }
-    
-    private class LoaderStub: FeedLoader {
-        private let result: FeedLoader.Result
-        
-        init(result: FeedLoader.Result) {
-            self.result = result
-        }
-        
-        func load(completion: @escaping (FeedLoader.Result) -> Void) {
-            completion(result)
-        }
     }
 }
 
