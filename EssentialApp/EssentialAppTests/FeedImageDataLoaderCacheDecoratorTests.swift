523a11d0d8e53b96c158c14cb9b122af947a8d5e
SwiftEnProfundidad <freelancemerlos@gmail.com>
2023-09-02 23:16:22 +0200

    Make the LocalFeedImageDataLoader implement the `FeedImageDataCache protocol (so it can be composed)

---
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
@@ -0,0 +1,8 @@
+//
+//  FeedImageDataLoaderCacheDecoratorTests.swift
+//  EssentialAppTests
+//
+//  Created by Juan Carlos Merlos Albarracin on 2/9/23.
+//
+
+import Foundation
---
+++ EssentialFeed/FeedFeature/FeedImageDataCache.swift
@@ -0,0 +1,8 @@
+//
+//  FeedImageDataCache.swift
+//  EssentialFeed
+//
+//  Created by Juan Carlos Merlos Albarracin on 2/9/23.
+//
+
+import Foundation
