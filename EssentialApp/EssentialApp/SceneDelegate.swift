d5b2808bc4b9691255ef738f89ac01000aaee8fa
SwiftEnProfundidad <freelancemerlos@gmail.com>
2023-09-03 11:36:13 +0200

    Remove duplicate files

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -18,23 +18,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
         
-        let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-        
-        let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
-        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
-        let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
-        
-        let localStoreURL = NSPersistentContainer
-            .defaultDirectoryURL()
-            .appendingPathComponent("feed-store.sqlite")
-        
-        let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
-        let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
-        let localImageLoader = LocalFeedImageDataLoader(store: localStore)
-        
-        window?.rootViewController = FeedUIComposer.feedComposedWith(
-            feedLoader: localFeedLoader,
-            imageLoader: localImageLoader)
+//        let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+//        
+//        let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+//        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
+//        let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
+//        
+//        let localStoreURL = NSPersistentContainer
+//            .defaultDirectoryURL()
+//            .appendingPathComponent("feed-store.sqlite")
+//        
+//        let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
+//        let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
+//        let localImageLoader = LocalFeedImageDataLoader(store: localStore)
+//        
+//        window?.rootViewController = FeedUIComposer.feedComposedWith(
+//            feedLoader: localFeedLoader,
+//            imageLoader: localImageLoader)
     }
 }
 
