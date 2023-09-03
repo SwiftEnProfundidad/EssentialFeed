c0f3edd79014f516c3a09405da63c81399bb1924
SwiftEnProfundidad <freelancemerlos@gmail.com>
2023-09-03 13:06:15 +0200

    Improve coverage by using identifiers for finding feed image cells

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -16,13 +16,15 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
         
         app.launch()
         
+        let feedCells = app.cells.matching(identifier: "feed-image-cell")
         // Si cargamos las 22 imágenes que nos vienen en el json remoto,
         // tienen que aparecer 22 celdas, solo probamos el cell count y
         // no que se carguen correctamente las imágenes en cada celda.
-        XCTAssertEqual(app.cells.count, 22)
+        XCTAssertEqual(feedCells.cells.count, 22)
         // Aquí nos aseguramos de que por lo menos se carga una imagen en la celda
         // que se muestra en pantalla, con lo que verificamos que se cargan
         // las imágenes en las celdas que aúno no están visibles en pantalla.
-        XCTAssertEqual(app.cells.firstMatch.images.count, 1)
+        let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
+        XCTAssertTrue(firstImage.exists)
     }
 }
